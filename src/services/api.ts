b2f64fadc250f0489239ed3a10c952d8d6ff8ba8
Joao Victor Angelo <146788838+JoaoVictorAngelo@users.noreply.github.com>
2026-08-20 17:20:09 -0300

    Update API base URL to new endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -36,7 +36,7 @@ type PurchaseResponse = {
 
 const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://ebac-fake-api.vercel.app/api/efood/'
+    baseUrl: 'https://api-ebac.vercel.app/api/efood/'
   }),
   endpoints: (builder) => ({
     getRestaurantes: builder.query<RestauranteDetalhado[], void>({
